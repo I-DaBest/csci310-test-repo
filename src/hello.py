40e32b7cbc01a58ba7de0113d7d009f6c385f07f
Katherine <khd001@morningside.edu>
2025-08-26 12:56:51 -1100

    new folder

---
+++ src/hello.py
@@ -0,0 +1,16 @@
+"""
+hello.py
+========
+This is a smaple script
+
+|Katherine Duong
+|2025 August 20
+"""
+
+print("hello world")
+
+def something() :
+    """
+    This function does nothing
+    """
+    pass
